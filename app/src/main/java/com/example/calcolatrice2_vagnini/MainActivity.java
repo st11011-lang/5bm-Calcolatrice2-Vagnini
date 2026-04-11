340224a11bc3ee1094ae9f7f5faf6afd9c5081e4
VAGNINI NATAN <st11011@iismarconipieralisi.it>
2026-04-11 09:55:15 +0200

    implementazione piu, meno e per

--- app/src/main/java/com/example/calcolatrice2_vagnini/MainActivity.java
+++ app/src/main/java/com/example/calcolatrice2_vagnini/MainActivity.java
@@ -18,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
     TextView testo;
     Button t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, piu, meno, per, diviso, punto, uguale, cancellaEntrata, cancellaTutto;
     ImageView immagine;
+
+    private double numero1 = 0;
+    private double numero2 = 0;
+    private double ris = 0;
+    private boolean operazioneInCorso = false;
+
+    private char operazione = '.';
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,80 +62,152 @@ public class MainActivity extends AppCompatActivity {
         t1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t1.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t1.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t1.getText().toString());
             }
         });
 
         t2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t2.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t2.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t2.getText().toString());
             }
         });
 
         t3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t3.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t3.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t3.getText().toString());
             }
         });
 
         t4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t4.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t4.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t4.getText().toString());
             }
         });
 
         t5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t5.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t5.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t5.getText().toString());
             }
         });
 
         t6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t6.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t6.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t6.getText().toString());
             }
         });
 
         t7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t7.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t7.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t7.getText().toString());
             }
         });
 
         t8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t8.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t8.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t8.getText().toString());
             }
         });
 
         t9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t9.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t9.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t9.getText().toString());
             }
         });
 
         t0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String numero = testo.getText().toString() + t0.getText().toString();
-                testo.setText(numero);
+                if(testo.getText().toString().equals("0"))
+                    testo.setText(t0.getText().toString());
+                else
+                    testo.setText(testo.getText().toString() + t0.getText().toString());
+            }
+        });
+
+        piu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numero1 = Double.parseDouble(testo.getText().toString());
+                operazioneInCorso = true;
+                operazione = '+';
+                testo.setText("");
+            }
+        });
+
+        meno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numero1 = Double.parseDouble(testo.getText().toString());
+                operazioneInCorso = true;
+                operazione = '-';
+                testo.setText("");
+            }
+        });
+
+        per.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numero1 = Double.parseDouble(testo.getText().toString());
+                operazioneInCorso = true;
+                operazione = '*';
+                testo.setText("");
+            }
+        });
+
+        uguale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (operazioneInCorso) {
+                    numero2 = Double.parseDouble(testo.getText().toString());
+                    if (operazione == '+') {
+                        ris = numero1 + numero2;
+                    } else if (operazione == '-') {
+                        ris = numero1 - numero2;
+                    } else if (operazione == '*') {
+                        ris = numero1 * numero2;
+                    }
+                    if (ris == Math.floor(ris)) {
+                        testo.setText(String.valueOf((long) ris));  // Mostra "5" invece di "5.0"
+                    } else {
+                        testo.setText(String.valueOf(ris));          // Mostra "5.5" se decimale
+                    }
+                    operazioneInCorso = false;
+                }
             }
         });
     }
